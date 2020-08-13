a7821e86e7da111a9976ec465f0a176b4b1f4a9f
Chaotic Guru <49621391+Chaotic-Lab@users.noreply.github.com>
2020-08-12 22:10:55 -0500

    Update and rename ACI.sh to ACBI.sh

--- ACBI.sh
+++ ACBI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Arch Chroot Install (ACI)
+# Arch Chroot BIOS Install (ACBI)
 # --------------------------------
 # Author    : ChaoticGuru
 # Github    : https://github.com/Chaotic-Lab
@@ -20,7 +20,7 @@ ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime #CHANGE THIS TO YOUR T
 
 #Install some needed packages
 pacman -Syyu
-pacman -S net-tools dhcpcd mlocate dnsutils zip ntfs-3g dialog wpa_supplicant sudo python3 python2 git wget curl grub efibootmgr dosfstools mtools os-prober reflector --noconfirm
+pacman -S net-tools dhcpcd mlocate dnsutils zip ntfs-3g dialog wpa_supplicant sudo python3 python2 git wget curl grub netctl neofetch os-prober reflector --noconfirm
 
 #Set root password
 echo "Please set ROOT password!!!"
@@ -38,6 +38,7 @@ grub-install --target=i386-pc /dev/sda --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
 #Successfully Installed
+neofetch
 echo "Arch Linux UEFI base has been successfully installed on your system..."
 echo "A reboot should now take place"
 echo "Run the following commands to reboot properly!"
